d7497e7cca32c698bc4b645c3113d083f221b2ad
michelleheh <xiaoyu.he22@gmail.com>
2016-04-03 17:27:34 -0700

    refactor link and user functions

--- app/models/link.js
+++ app/models/link.js
@@ -4,28 +4,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// ==========================================
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function() {
-//     this.on('creating', function(model, attrs, options) {
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-// ==========================================
-
 var linkSchema = new Schema({
-  username: String,
-  url: String
+  url: String ,
+  base_url: String ,
+  code: String,
+  title: String,
+  visits: Number,
 });
 
 var Link = mongoose.model('Link', linkSchema);
+
+// var createShasum = function(url) {
+//   var shasum = crypto.createHash('sha1');
+//   shasum.update(url);
+//   return shasum.digest('hex').slice(0, 5);
+// };
+
+// linkSchema.pre('save', function(next) {
+//   this.code = createShasum(this.url);
+//   next();
+// });
 
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -1,17 +1,16 @@
-// var db = require('../config');
-// var bcrypt = require('bcrypt-nodejs');
-// var Promise = require('bluebird');
-
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var bluebird = require('bluebird');
+
 var Schema = mongoose.Schema;
 
 // ==========================================
 // var User = db.Model.extend({
 //   tableName: 'users',
 //   hasTimestamps: true,
-//   initialize: function() {
-//     this.on('creating', this.hashPassword);
-//   },
+  // initialize: function() {
+  //   this.on('creating', this.hashPassword);
+  // },
 //   comparePassword: function(attemptedPassword, callback) {
 //     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
 //       callback(isMatch);
@@ -28,11 +27,39 @@ var Schema = mongoose.Schema;
 // ==========================================
 
 var userSchema = new Schema({
-  username: String,
-  password: String,
-  url: String
+  // Mongoose assigns each of your schemas an _id field by default if one is not passed into the Schema constructor. 
+  username: { type: String , unique: true },
+  password: String
 });
 
 var User = mongoose.model('User', userSchema);
 
+// User.prototype.comparePassword = function(attemptedPassword, callback) {
+//   bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+//     callback(isMatch);
+//   });
+// };
+
+// userSchema.pre('save', function(next) {
+//   var cipher = Promise.promisify(bcrypt.hash);
+//   return cipher(this.password, null, null).bind(this)
+//     .then(function(hash) {
+//       this.password = hash;
+//       next();
+//     });
+// });
+
 module.exports = User;
+
+//   comparePassword: function(attemptedPassword, callback) {
+//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+//       callback(isMatch);
+//     });
+//   },
+//   hashPassword: function() {
+//     var cipher = Promise.promisify(bcrypt.hash);
+//     return cipher(this.get('password'), null, null).bind(this)
+//       .then(function(hash) {
+//         this.set('password', hash);
+//       });
+//   }
